bc4e14580e2c19268a6617358feb18bfb867700f
Dave Brock <daveabrock@gmail.com>
2021-02-18 06:39:03 -0600

    Add project files.

---
+++ Services/ClipboardService.cs
@@ -0,0 +1,21 @@
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace ClipboardSample.Services
+{
+    public class ClipboardService
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public ClipboardService(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public ValueTask WriteTextAsync(string text)
+        {
+            return _jsRuntime.InvokeVoidAsync("navigator.clipboard.writeText", text);
+        }
+    }
+}
+
